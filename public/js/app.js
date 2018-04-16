5eeb4868da4ab505113a64b194c3862c0744c5e9
harrycaveman <hjoseph@unca.edu>
2018-04-16 00:17:59 -0400

    new style that I like better

--- public/js/app.js
+++ public/js/app.js
@@ -1,42 +1,52 @@
+
+const addnote = note => $("#notes").prepend(
+            $('<li></li>').text(`${note.author}:`).append(
+            $("<li></li>").text(note.body).append('<hr>'))
+            )
+
 //listener for comments buttons
 /*
 *
 *@event: the listener event
 *@@this: the comment button
 */
-const addnote = note => $("#notes").prepend(
-            $('<li></li>').text(`${note.author}:`).append(
-            $("<li></li>").text(note.body).append('<hr>'))
-            )
-
-
 $(document).on("click",".open-comments", function(event) {
+	//No, window! No reload!
 	event.preventDefault();
 	$("#comment-section").empty();
+	//drab id from button
 	let id = $(this).attr("data-id");
-
+    //get article and populate chat with comments
 	$.get(`/api/article/${id}`)
 	.done((data,status,xhr) => { 
 		//comment section heading with headline
 		$("#comment-section").append("<h2><i>Comments</i></h2>");
-	    $("#comment-section").append(`<h3><a href=${data.link} target="blank"><i>${data.title}</i></a></h3>`);
+	    $("#comment-section").append(`<h3 id="comments-caption"><a href=${data.link} target="blank"><i>${data.title}</i></a></h3>`);
 		 // A textarea to add a new note body
         $("#comment-section").append("<textarea id='bodyinput' name='body' placeholder='Type your message here'></textarea>");
 		//text input for commenter name (for now)
 		$("#comment-section").append("<input id='authorinput' name='title' placeholder='Your name (required)' required>");		
 		// A button to submit a new note, with the id of the article saved to it
-		$("#comment-section").append("<button data-id='" + data._id + "' class='btn btn-success post-comment'>Post Comment</button>");
-        $("#comment-section").append("<div><ul id='notes'></ul></div>");
+		$("#comment-section").append("<button id='post-comment' data-id='" + data._id + "' class='btn btn-success'>Post Comment</button>");
+        $("#comment-section").append("<div id='note-wrapper'><ul id='notes'></ul></div>");
 		data.notes.forEach(note => addnote(note));
 	})
 	.fail(err => console.log(err));
 
 });
 
+//listener for comment post button
+/*
+*
+*@event: the listener event
+*@@this: the comment post button
+*/
 $(document).on("click",".post-comment", function(event){
+	//I thought I told you not to Mr. window
 	event.preventDefault();
+	//grab id from button
 	let id = $(this).attr("data-id");
-
+     
 	let note = {
       author:$("#authorinput").val(),
       body:$("#bodyinput").val()
